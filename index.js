505c0d8c7134d2e10b577c669e8a6a543b5a2fe7
Sohaib Ashraf Butt <sohaibbutt57@gmail.com>
2023-02-14 01:03:45 +0500

    using callbacks node flow implemented

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 const http = require('http');
 const url = require('url');
-const async = require('async');
 const request = require('request');
 
 const server = http.createServer((req, res) => {
-    
   if (req.url.startsWith('/I/want/title/') && req.method === 'GET') {
     const queryData = url.parse(req.url, true).query;
     const addresses = Array.isArray(queryData.address) ? queryData.address : [queryData.address];
 
+    let results = [];
+    let completed = 0;
 
-    async.map(addresses, (address, callback) => {
+    addresses.forEach((address, index) => {
       const addressUrl = !/^https?:\/\//i.test(address) ? `http://${address}` : address;
       request({ url: addressUrl, followRedirect: true }, (error, response, body) => {
         if (error) {
-          callback(null, { address, title: 'No Response' });
-          return;
+          results[index] = { address, title: 'No Response' };
+        } else {
+          const title = body.match(/<title>(.*?)<\/title>/i);
+          results[index] = { address, title: title ? title[1] : 'No Response' };
         }
+        completed += 1;
 
-        const title = body.match(/<title>(.*?)<\/title>/i);
-        callback(null, { address, title: title ? title[1] : 'No Response' });
+        if (completed === addresses.length) {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write('<html><head></head><body><h1> Following are the titles of given websites: </h1><ul>');
+          results.forEach(result => {
+            res.write(`<li>${result.address} - "${result.title}"</li>`);
+          });
+          res.end('</ul></body></html>');
+        }
       });
-    }, (error, results) => {
-      if (error) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-        return;
-      }
-
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write('<html><head></head><body><h1> Following are the titles of given websites: </h1><ul>');
-      results.forEach((result) => {
-        res.write(`<li>${result.address} - "${result.title}"</li>`);
-      });
-      res.end('</ul></body></html>');
     });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
